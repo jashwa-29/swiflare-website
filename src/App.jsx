e5bab7d012480327f772c008eb139e3ad711e8e6
jashwa-29 <153584496+jashwa-29@users.noreply.github.com>
2025-05-01 21:34:03 +0530

    Your update message

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './Pages/Home'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import Footer from './Components/CommonComponents/Footer';
+import About from './Pages/About';
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
     <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<h1 className='text-3xl font-bold underline'>About</h1>} />
+      <Route path="/about" element={<About/>} />
       <Route path="/contact" element={<h1 className='text-3xl font-bold underline'>Contact</h1>} />
     </Routes>
     <Footer/>
